11deca5570f0343675a4a64f4382256eaa92db9e
JieunKim <78676660+mellykim123@users.noreply.github.com>
2024-05-15 09:01:26 +0900

    fix: 문구수정

--- src/components/LoadingBar/index.tsx
+++ src/components/LoadingBar/index.tsx
@@ -4,7 +4,7 @@ import { StyledLoadingBarWrapper } from "./style";
 export const LoadingBar = () => {
   return (
     <StyledLoadingBarWrapper>
-      요청하신 이미지를 생성중입니다.
+      요청하신 이미지 생성 대기중입니다
       <Spin indicator={<LoadingOutlined style={{ fontSize: 24 }} spin />} />
     </StyledLoadingBarWrapper>
   );
